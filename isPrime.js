5b0030e826a6b16d0522b075dd7211597470ddc9
akankshadhanewar <55694846+akankshadhanewar@users.noreply.github.com>
2020-05-29 16:19:07 +0530

    Create isPrime.js

---
+++ isPrime.js
@@ -0,0 +1,26 @@
+function test_prime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+
+var assert = require("assert");
+assert(test_prime(10) == true, "This is not a prime num");
+console.log(test_prime(37));
